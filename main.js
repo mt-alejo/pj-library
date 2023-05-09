cdfdb8f6069543da41c47d58f5c1d850bcd86220
Ariel Alejandro <axelalejopc123@gmail.com>
2023-05-09 15:49:44 -0500

    [STEP 3] Create a function that saves the user's input

--- main.js
+++ main.js
@@ -9,7 +9,15 @@ class Book {
   }
 }
 
-const addBookToLibrary = () => {};
+const addBookToLibrary = () => {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+};
 
 const formContainer = document.querySelector("#add-book-form");
 const btnAddBook = document.querySelector("#add-book-btn");
