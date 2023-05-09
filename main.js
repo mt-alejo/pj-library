54146732eaa7aa99f42cb22be846847e914f30c2
Ariel Alejandro <axelalejopc123@gmail.com>
2023-05-09 15:23:40 -0500

    [STEP 1] Define the constructor

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert("Works");
+const myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, language, read) {
+    (this.title = title),
+      (this.author = author),
+      (this.pages = pages),
+      (this.language = language),
+      (this.read = read);
+  }
+}
+
+const addBookToLibrary = () => {};
